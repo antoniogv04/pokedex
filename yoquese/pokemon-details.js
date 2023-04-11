ef411517474ea0485e67497c1d28c1f8271a0d8e
marinalp04 <arriagamarina4@gmail.com>
2023-04-11 18:20:54 +0200

    .

--- yoquese/pokemon-details.js
+++ yoquese/pokemon-details.js
@@ -64,3 +64,63 @@ function fetchPokemonDetails(pokemonName) {
     pokemonStats.appendChild(statContainer);
   });
   
+  fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`)
+  .then(response => response.json())
+  .then(data => {
+    let evolutionChainUrl = data.evolution_chain.url;
+    fetch(evolutionChainUrl)
+      .then(response => response.json())
+      .then(data => {
+        let chain = data.chain;
+        let evolutionChain = document.createElement("div");
+        evolutionChain.classList.add("evolution-chain");
+        createEvolutionChain(chain, evolutionChain);
+        // Aquí se muestra la cadena evolutiva del pokémon
+      });
+  });
+
+// Obtener el ID del Pokémon deseado
+const pokemonId = 25; // Pikachu
+
+// Hacer una solicitud a la API de PokéAPI para obtener la información del Pokémon
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+  .then(response => response.json())
+  .then(data => {
+    const evolutionChainUrl = data.species.url; // Obtener la URL de la cadena evolutiva
+    
+    // Hacer una solicitud a la API de PokéAPI para obtener la cadena evolutiva
+    fetch(evolutionChainUrl)
+      .then(response => response.json())
+      .then(data => {
+        const chain = data.chain; // Obtener la cadena evolutiva
+        
+        // Mostrar la cadena evolutiva en la página web
+        const evolutionChainElement = document.getElementById('evolution-chain');
+        evolutionChainElement.innerHTML = '';
+
+        let currentPokemon = chain.species;
+        while (currentPokemon) {
+          const name = currentPokemon.name;
+          const url = currentPokemon.url;
+          const trigger = currentPokemon.evolution_trigger || 'Nivel'; // Obtener el trigger de evolución o establecer el valor por defecto "Nivel"
+          
+          // Crear un elemento <a> para cada Pokémon en la cadena evolutiva
+          const pokemonElement = document.createElement('a');
+          pokemonElement.href = url;
+          pokemonElement.textContent = name;
+          
+          // Añadir un evento de clic para mostrar la información detallada del Pokémon
+          pokemonElement.addEventListener('click', (event) => {
+            event.preventDefault();
+            // Obtener la información detallada del Pokémon y mostrarla en la página web
+            // ...
+          });
+          
+          // Añadir el elemento <a> al contenedor de la cadena evolutiva
+          evolutionChainElement.appendChild(pokemonElement);
+          
+          // Obtener el siguiente Pokémon en la cadena evolutiva
+          currentPokemon = currentPokemon.evolves_to[0];
+        }
+      });
+  });
